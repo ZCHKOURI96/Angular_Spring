a32bd68a33a3bce43011e1c0b98ba4e733b8b3dc
Moad Aitaayi <majdaitaayi@gmail.com>
2024-01-20 20:20:35 +0100

    delete les relations entre l'admin, assistant et planification

--- src/main/java/com/gestion/formation/entity/Admin.java
+++ src/main/java/com/gestion/formation/entity/Admin.java
@@ -1,24 +1,12 @@
 package com.gestion.formation.entity;
 
-import java.util.List;
-
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @DiscriminatorValue("ADMIN")
 public class Admin extends User {
-    
-    @OneToMany(mappedBy = "admin")
-    private List<Planification> planifications;
 
 }
--- src/main/java/com/gestion/formation/entity/Assistant.java
+++ src/main/java/com/gestion/formation/entity/Assistant.java
@@ -1,24 +1,10 @@
 package com.gestion.formation.entity;
 
-import java.util.List;
-
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @DiscriminatorValue("assistant")
 public class Assistant extends User {
-
-    @OneToMany(mappedBy = "assistant")
-    private List<Planification> planifications;
 
 }
